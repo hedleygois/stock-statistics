0b1dd64fa8d10425a24436bee8c0751e79c2a253
Hedley Luna <hluna@stackstate.com>
2019-08-08 20:47:22 +0200

    Creating some data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO Stocks (id, ticker, company, value, period) VALUES (1, 'ITSA4', 'Itausa', 12.45, '2019-07-31');
+INSERT INTO Stocks (id, ticker, company, value, period) VALUES (2, 'TAEE11', 'Taesa', 27.65, '2019-07-31');
+INSERT INTO Stocks (id, ticker, company, value, period) VALUES (3, 'PETR4', 'Petrobras', 26.49, '2019-07-31');
+INSERT INTO Stocks (id, ticker, company, value, period) VALUES (4, 'SAPR11', 'Sanepar', 84.18, '2019-07-31');
+INSERT INTO Stocks (id, ticker, company, value, period) VALUES (5, 'RAIL3', 'Rumo', 21.67, '2019-07-31');
+INSERT INTO Stocks (id, ticker, company, value, period) VALUES (6, 'IRBR3', 'Instituto de Resseguros Brasil', 93.44, '2019-07-31');
+INSERT INTO Stocks (id, ticker, company, value, period) VALUES (7, 'BBAS3', 'Banco do Brasil', 49.50, '2019-07-31');
+INSERT INTO Stocks (id, ticker, company, value, period) VALUES (8, 'SMAL11', 'SmallCaps', 110.69, '2019-07-31');
+INSERT INTO Stocks (id, ticker, company, value, period) VALUES (9, 'CMIG4', 'Cemig', 14.14, '2019-07-31');
+INSERT INTO Stocks (id, ticker, company, value, period) VALUES (10, 'POMO4', 'Marcopolo', 3.81, '2019-07-31');
+INSERT INTO Stocks (id, ticker, company, value, period) VALUES (11, 'HBOR3', 'Helbor', 2.24, '2019-07-31');
+INSERT INTO Stocks (id, ticker, company, value, period) VALUES (12, 'OIBR3', 'OI', 1.54, '2019-07-31');
+INSERT INTO Stocks (id, ticker, company, value, period) VALUES (13, 'B3SA3', 'B3', 42.04, '2019-07-31');
